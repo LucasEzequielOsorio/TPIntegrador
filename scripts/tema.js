29cc8429e18390d629a262a874f7c2df739c62a5
LucasEzequielOsorio <lucasezequiel04@gmail.com>
2024-09-19 16:55:57 -0300

    Acabado header

--- scripts/tema.js
+++ scripts/tema.js
@@ -1,7 +1,19 @@
 const header = `
-    <div class="logo"></div>
-    <div class="search_bar"></div>
-    <div class="cart_logo"></div>`;
+    <a href="../index.html" class="logo">
+        <span class="logo_black">Cours</span><span class="logo_blue">Ed</span>
+    </a>
+    <form class="search_bar">
+        <input list="courses" name="course" id="course">
+            <datalist id="courses">
+                <option value="Curso Presencial 1">
+                <option value="Curso virtual 1">
+                <option value="Curso presencial 2">
+                <option value="Curso virtual 2">
+                <option value="Curso presencial 3">
+            </datalist>
+        <button type="submit">Buscar</button>
+    </form>
+    <a href="#" class="cart_logo"><span>0</span></a>`;
 const footer = `
     <div class="footer_item">
         <h4>El grupo</h4>
@@ -13,7 +25,7 @@ const footer = `
         </ul>
     </div>
     <div class="footer_item">
-        <h4>El grupo</h4><p>asdasdasda</p>
+        <h4>El grupo</h4><p>lorem ipsum</p>
     </div>
     <div class="footer_item">
         <h4>Seguinos</h4>
