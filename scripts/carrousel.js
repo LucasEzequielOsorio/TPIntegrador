4a4080607c73f794f4781e7a7eb4ac0b6931bbd9
lucasfedericogarcia <l.f.g_12@hotmail.com>
2024-11-03 17:56:02 -0300

    se arreglo carrousel en responsive y botones de admin junto con @franmgonzalez
    se arreglo el paso de las diapositivas para responsive y el posicionamiento de los botones de adminPerfil para responsive
    
    Co-Authored-By: franmgonzalez <180598050+franmgonzalez@users.noreply.github.com>

--- scripts/carrousel.js
+++ scripts/carrousel.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     const carrousel = document.querySelector(".carrousel");
     const cursos = Array.from(carrousel.children);
     let currentIndex = 0;
-    const visibleCursos = 3; // Número de cursos visibles al mismo tiempo
-    const delay = 3000; // Tiempo en milisegundos entre cada cambio
+    const visibleCursos = 3;
+    const delay = 2000; // Tiempo en milisegundos entre cada cambio
 
     function updateCarrousel() {
         // Ocultar todos los cursos primero
@@ -25,11 +25,56 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     
+    
     // Iniciar el carrusel automático
     setInterval(nextSlide, delay);
 
     // Mostrar los primeros cursos al cargar la página
     updateCarrousel();
     
-    
+}); */
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carrousel = document.querySelector(".carrousel");
+    const cursos = Array.from(carrousel.children);
+    let currentIndex = 0;
+    let visibleCursos = 1;
+    const delay = 3000;
+
+    // ajustar el número de cursos visibles en función de la resolución
+    function adjustVisibleCursos() {
+        if (window.matchMedia("(min-width: 1025px)").matches) {
+            visibleCursos = 3;
+        } else {
+            visibleCursos = 1;
+        }
+        updateCarrousel(); // Actualizar el carrusel cuando cambia el número de elementos visibles
+    }
+
+    function updateCarrousel() {
+        // Ocultar todos los cursos primero
+        cursos.forEach(curso => {
+            curso.style.display = "none";
+        });
+
+        // Mostrar solo los cursos visibles
+        for (let i = 0; i < visibleCursos; i++) {
+            const cursoIndex = (currentIndex + i) % cursos.length;
+            cursos[cursoIndex].style.display = "flex";
+        }
+    }
+
+    function nextSlide() {
+        // Cambiar al siguiente índice y actualizar el carrusel
+        currentIndex = (currentIndex + 1) % cursos.length;
+        updateCarrousel();
+    }
+
+    // Iniciar el carrusel automático
+    setInterval(nextSlide, delay);
+
+    // Ajustar el número de cursos visibles al cargar la página y al cambiar el tamaño de la ventana
+    adjustVisibleCursos();
+    window.addEventListener("resize", adjustVisibleCursos);
 });
